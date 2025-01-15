f19841622631d69991774376f331b1af60c670c1
Isael Diroche <isaeldiroche00@gmail.com>
2025-01-15 13:41:27 -0400

    Update forwardBack.tsx

--- src/shared/navigation/forwardBack.tsx
+++ src/shared/navigation/forwardBack.tsx
@@ -9,17 +9,13 @@ const ForwardBackButtons: React.FC = () => {
 
     useEffect(() => {
         const updateNavigationState = () => {
-            setCanGoBack(window.history.state !== null && window.history.length > 1);
-            setCanGoForward(false); // La API del navegador no ofrece un acceso directo al estado de "puedo ir hacia adelante"
+            setCanGoBack(window.history.state !== null);
+            setCanGoForward(window.history.length > 1);
         };
 
-        // Actualiza el estado inicial al montar el componente
+        window.addEventListener('popstate', updateNavigationState);
         updateNavigationState();
 
-        // Escucha eventos de navegación (hacia atrás/adelante)
-        window.addEventListener('popstate', updateNavigationState);
-
-        // Limpia el evento al desmontar
         return () => {
             window.removeEventListener('popstate', updateNavigationState);
         };
@@ -27,22 +23,16 @@ const ForwardBackButtons: React.FC = () => {
 
     return (
         <div className="flex items-center gap-5">
-            {/* Botón para ir hacia atrás */}
             <button
                 onClick={() => canGoBack && window.history.back()}
-                disabled={!canGoBack} // Deshabilita si no se puede ir hacia atrás
-                className={`transition-opacity ${canGoBack ? 'opacity-100 cursor-pointer' : 'opacity-50 cursor-not-allowed'}`}
+                className={`transition-opacity ${canGoBack ? 'opacity-100 cursor-pointer' : 'opacity-50 cursor-default'}`}
                 type="button"
             >
                 <BiArrowBack className="w-4 h-4 text-gray-800" />
             </button>
-
-            {/* Botón para ir hacia adelante */}
             <button
                 onClick={() => canGoForward && window.history.forward()}
-                disabled={!canGoForward} // Deshabilita si no se puede ir hacia adelante
-                className={`transition-opacity ${canGoForward ? 'opacity-100 cursor-pointer' : 'opacity-50 cursor-not-allowed'}`}
-                type="button"
+                className={`transition-opacity ${canGoForward ? 'opacity-100 cursor-pointer' : 'opacity-50 cursor-default'}`}
             >
                 <BiArrowBack className="w-4 h-4 text-gray-800 rotate-180" />
             </button>
